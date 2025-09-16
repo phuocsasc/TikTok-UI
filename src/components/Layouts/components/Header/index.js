c858dc56bd7015b8a44a750f9b58fd51b1b001ab
phuocsasc <phuoctran.22102004@gmail.com>
2025-09-16 13:13:15 +0700

    Xây dựng UI phần Header #1

--- src/components/Layouts/components/Header/index.js
+++ src/components/Layouts/components/Header/index.js
@@ -1,12 +1,30 @@
 import classNames from 'classnames/bind';
 import styles from './Header.module.scss';
+import images from '~/assets/images';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 const css = classNames.bind(styles);
 
 function Header() {
     return (
         <header className={css('wrapper')}>
-            <div className={css('inner')}></div>
+            <div className={css('inner')}>
+                <div className={css('logo')}>
+                    <img src={images.logo} alt="TikTok"></img>
+                </div>
+                <div className={css('search')}>
+                    <input placeholder="Search accounts and videos" spellCheck={false} />
+                    <button className={css('clear')}>
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button>
+                    <FontAwesomeIcon className={css('loading')} icon={faSpinner} />
+                    <button className={css('search-btn')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
+                </div>
+                <div className={css('actions')}>actions</div>
+            </div>
         </header>
     );
 }
